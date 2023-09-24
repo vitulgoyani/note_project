926aff035b977cb5ae64b84156cb5165a89e003e
vitul <vitulgoyani@gmail.com>
2023-09-24 12:30:06 +0530

    this is rtest

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
+    } else if (req.accepts('json')) { 
         res.json({ message: '404 not found' })
     } else {
         res.type('txt').send('404 not found')
