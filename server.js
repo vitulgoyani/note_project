77c0bec440d4d349086f31c445fc23bec72918ab
vitul <vitulgoyani@gmail.com>
2023-09-26 10:43:06 +0530

    add middleware

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const PORT = process.env.PORT || 3500
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const PORT = process.env.PORT || 3500;
 
+app.use(logger);
 
-app.use("/", express.static(path.join(__dirname, "public")))
+app.use(cors(corsOptions));
 
-app.use("/", require("./routes/root"))
+app.use(express.json());
 
-app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) { 
-        res.json({ message: '404 not found' })
-    } else {
-        res.type('txt').send('404 not found')
-    }
-})
+app.use(cookieParser());
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+/// it is for use of public folder
+app.use("/", express.static(path.join(__dirname, "public")));
 
+/// here all root start
+app.use("/", require("./routes/root"));
+
+/// it is for 404 perpose
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+app.use(errorHandler);
+
+/// this is for listen on port
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
